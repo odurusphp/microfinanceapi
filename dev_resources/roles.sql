0cf05f0ed60cdf064831964d792e2bd61e16d16d
odurusphp <odurusphp@gmail.com>
2019-12-09 13:21:00 +0000

    roles tabel added

---
+++ dev_resources/roles.sql
@@ -0,0 +1,31 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.7.21-log : Database - rogg
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `roles` */
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `roleid` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(24) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`roleid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `roles` */
+
+insert  into `roles`(`roleid`,`role`,`description`) values (1,'Administrator','Administrator'),(2,'Customer_Administrator','Client account'),(3,'Customer_User','Users of Custpomers');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
