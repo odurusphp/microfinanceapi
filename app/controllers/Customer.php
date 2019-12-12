41ee2537265fb40ed55c2d49f3d84543be39b6c1
odurusphp <odurusphp@gmail.com>
2019-12-12 03:24:25 +0000

    get customer data method

---
+++ app/controllers/Customer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oduru
+ * Date: 12/12/2019
+ * Time: 3:21 AM
+ */
+
+class Customer extends Controller
+{
+    public function details($basicid = null)
+    {
+        $rs = new RestApi();
+
+        // Verify Apikey
+        $rs->getApikey();
+
+        //Getting Authorization token
+        $token = $rs->getBearerToken();
+
+        //Verifying Token
+        $rs->verifyToken($token);
+
+
+        //Getting the actual Api method
+        $us = new Basicinformation($basicid);
+        $basicdata = $us->recordObject;
+
+        $alluserdata = ['basicdata'=>$basicdata];
+
+        $rs->returnResponse($alluserdata);
+
+    }
+}
