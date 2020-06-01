f09a8c7c4454923d55905e27f631d67897849db4
Prince Oduro <prince@getinnotized.com>
2020-06-01 18:56:29 +0100

    test azubi sms

--- app/controllers/Sms.php
+++ app/controllers/Sms.php
@@ -7,7 +7,7 @@ class Sms extends Controller
      public function index(){
 
          //$sm =  Smslog::getPendingLog();
-//         $smid = $sm->smsid;
+//        $smid = $sm->smsid;
 //         $telephone = $sm->telephone;
          $telephone = '209155165';
           textazubi($telephone);
